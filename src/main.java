d0d7b077725dc66eeebd2eda7affab4a344d52db
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2022-07-22 11:32:22 -0400

    Added access to API

---
+++ src/main.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class main {
+    
+    private static HttpURLConnection connection;
+    
+    public static void main(String[] args) {
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://movies-app1.p.rapidapi.com/api/genres"))
+                    .header("X-RapidAPI-Key", "cee54a15cdmsh111590b419bfebcp182fd4jsnf2c5965ba2dc")
+                    .header("X-RapidAPI-Host", "movies-app1.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+            // Request setup
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
