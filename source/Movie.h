6a4c1482cd1c9e05354022e949e725f86fd4280e
kylonocom <87710102+kylonocom@users.noreply.github.com>
2022-07-22 16:53:01 -0400

    Create Movie.h

---
+++ source/Movie.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Movie {
+private:
+    int id;
+    string title;
+    float voteAverage;
+    vector<string> genre;
+public:
+    Movie(int id, string title, float voteAverage);
+    void printData();
+};
+
+// Your Watchlist:
+// Pulp Fiction (9.8) (Action, Crime)
