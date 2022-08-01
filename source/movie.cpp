c600ac4c742b567538a07cf422589fa945819442
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2022-07-31 22:14:55 -0400

    Create movie.cpp

---
+++ source/movie.cpp
@@ -0,0 +1,25 @@
+#include "movie.h"
+#include <iostream>
+
+Movie::Movie(int id, string title, float voteAverage)
+{
+    this->id = id;
+    this->title = title;
+    this->voteAverage = voteAverage;
+}
+
+// Prints the voting Average and the genres of the movie
+void Movie::printData()
+{
+    std::cout << title << " (" << voteAverage << ") " << "(";
+
+    for(int i = 0; i < genre.size(); ++i)
+    {
+        if (i == genre.size() - 1)
+            std::cout << genre[i];
+        else
+            std::cout << genre[i] << ", ";
+    }
+
+    std::cout << ")\n";
+}
